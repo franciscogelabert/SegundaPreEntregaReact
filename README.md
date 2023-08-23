23e45957ee5095ee3d631b7ae74ab039b4ab9c3a
franciscogelabert <50490304+franciscogelabert@users.noreply.github.com>
2023-08-23 13:57:30 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SegundaPreEntregaReact
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-u6argz)
---
+++ src/components/main/Greeting.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { IconChefHat } from '@tabler/icons-react';
+import { IconConfetti } from '@tabler/icons-react';
+
+import ProductDetail from '../product/ProductDetail.jsx';
+
+const Greeting = (props) => {
+  return (
+    <main>
+      <div className="container-fluid py-2 justify-content-end">
+        <div id="publicaciones" className="row">
+          <div className="card">
+            <h1>
+              {' '}
+              {props.titulo1} <IconConfetti color="black" size={60} />{' '}
+            </h1>
+          </div>
+          <div className="card">
+            <h2>
+              {props.titulo2} <IconChefHat color="black" size={60} />
+            </h2>
+          </div>
+          <ProductDetail />
+        </div>
+      </div>
+    </main>
+  );
+};
+export default Greeting;
